d50d227f80ff569fbbfbd6fcb3a2f6e59fa65009
Tung Luu <luukhanhtung2002@gmail.com>
2019-05-16 22:53:39 +0700

    Adding some controlling function for player

--- client/src/components/Tank.js
+++ client/src/components/Tank.js
@@ -1,13 +1,45 @@
 class Tank  {
-    constructor(x, y, radius, speed, maxSpeed, acceleratingSpeed, turnSpeed, color) {
-        this.pos = createVector(x,y);
+    constructor(x, y, radius, maxSpeed, acceleratingSpeed, turnSpeed, color) {
+        this.pos = createVector(x, y);
+        this.currentSpeed = createVector(0, 0);
         this.radius = radius;
-        this.speed = speed;
         this.maxSpeed = maxSpeed;
         this.acceleratingSpeed = acceleratingSpeed;
         this.turnSpeed = turnSpeed;
         this.color = color;
+        this.angle = 2*PI;
     }
+
+    constrainSpeed() {
+        if(this.currentSpeed.mag() > this.maxSpeed) {
+            let currentVector = this.currentSpeed;
+            currentVector.normalize(); // vector's length equal to 1 without changing the ratio between x and y
+            this.currentSpeed.x = currentVector.x * this.maxSpeed;
+            this.currentSpeed.y = currentVector.y * this.maxSpeed;
+        }
+    }
+
+    turn() {
+        this.angle += this.turnSpeed;
+    }
+    
+    handleX() {
+        this.pos.x += this.currentSpeed.x;
+        this.pos.x = this.pos.x > canvas.width ? canvas.width : this.pos.x;
+        this.pos.x = this.pos.x < 0 ? 0 : this.pos.x;
+    }
+
+    handleY() {
+        this.pos.y += this.currentSpeed.y;
+        this.pos.y = this.pos.y > canvas.height ? canvas.height : this.pos.y;
+        this.pos.y = this.pos.y < 0 ? 0 : this.pos.y;
+    }
+
+    handleMovement() {
+        handleX();
+        handleY();
+    }
+
     draw() {
         push();
         strokeWeight(4);
