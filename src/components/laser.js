cb3eefb1e00370bbe15d6a98ac3bce44c3ff4a93
unknown <luukhanhtung2002@gmail.com>
2020-01-25 22:53:12 +0700

    Basically complete the laser

--- src/components/laser.js
+++ src/components/laser.js
@@ -3,6 +3,7 @@ class Laser {
         this.width = width
         this.id = id
         this.tankPos = {x: tanks[id].pos.x, y: tanks[id].pos.y}
+        this.intialAngle = tanks[id].angle
         this.direction = p5.Vector.fromAngle(tanks[id].angle);
         this.startingPos = utils.returnPosByDirection(tanks[id], this.direction)
         this.slope = this.direction.y / this.direction.x
@@ -15,11 +16,15 @@ class Laser {
             c: y0
         }
     }
-    calcSectPointLaserVsWalls() {
-        let { x, y} = this.direction 
-        let y0 = this.tankPos.y - this.slope * this.tankPos.x
-        let laser = {a1: this.slope, b1: -1, c1: y0}
+    calcSectPointLaserVsWalls(startingPos, direction) {
+        let { x, y } = direction 
+        let slope = y/x;
+        let y0 = startingPos.y - slope * startingPos.x
+        let laser = {a1: slope, b1: -1, c1: y0}
         let intersectingPoint
+        // When the vector point into one of these four quadrants, there will be two lines that 
+        // it can intersect. Therefore, we need two "if" statements to see eliminate the line
+        // whose intersection is out of border.
         if(x > 0 && y < 0){  //Quadrant 1
             intersectingPoint = utils.calcIntersectingPoints(laser, {a2: 1, b2: 0, c2: -canvas.width})
             if(intersectingPoint.y < 0) {
@@ -47,18 +52,26 @@ class Laser {
         else if( y === 0 && x > 0)
             intersectingPoint = {x: canvas.width, y: y0}
         else if( x === 0 && y < 0)
-            intersectingPoint = {x: this.startingPos.x, y: 0}
+            intersectingPoint = {x: startingPos.x, y: 0}
         else if( y === 0 && x < 0)
             intersectingPoint = {x: 0, y: y0}
         else if( x === 0 && y > 0)
-            intersectingPoint = {x: this.startingPos.x, y: canvas.height}
+            intersectingPoint = {x: startingPos.x, y: canvas.height}
         return intersectingPoint
     }
+    //Bug
     draw() {
-        let test = this.calcSectPointLaserVsWalls()
+        let firstDestination = this.calcSectPointLaserVsWalls(this.startingPos, this.direction)
+        let secondAngle
+        if(firstDestination.x === 0 || firstDestination.x === canvas.width)
+            secondAngle = -(sketch1.PI + this.intialAngle)
+        else if(firstDestination.y === 0 || firstDestination.y === canvas.height) 
+            secondAngle = -this.intialAngle
+        let secondDestination = this.calcSectPointLaserVsWalls(firstDestination, p5.Vector.fromAngle(secondAngle))
         sketch1.push()
         sketch1.strokeWeight(this.width)
-        sketch1.line(this.startingPos.x, this.startingPos.y, test.x, test.y)
+        sketch1.line(this.startingPos.x, this.startingPos.y, firstDestination.x, firstDestination.y)
+        sketch1.line(firstDestination.x, firstDestination.y, secondDestination.x, secondDestination.y)
         sketch1.pop()
     }
 }
